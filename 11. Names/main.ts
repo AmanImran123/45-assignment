d59a363ca611a3b26a9a723e224b3cf9f8fbcf3f
AmanImran123 <167163491+AmanImran123@users.noreply.github.com>
2024-04-25 20:10:52 +0500

    Add files via upload

---
+++ 11. Names/main.ts
@@ -0,0 +1,6 @@
+
+
+let members: string[] = ['ali', 'atif', 'shadan', 'sara'];
+for (let index = 0; index < members.length; index++) {
+  console.log(members[index]);
+}
---
+++ 12.Greetings/main.ts
@@ -0,0 +1,6 @@
+// exercise 12
+let members: string[] = ['ali', 'atif', 'shadan', 'sara'];
+let message: string='kal kon se class hai';
+for (let index = 0; index < members.length; index++) {
+  console.log(message + members[index]);
+}
